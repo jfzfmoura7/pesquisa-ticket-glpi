cfff2c695c9b53afef6ff4e25313590e891bea52
jfzfmoura7 <157635262+jfzfmoura7@users.noreply.github.com>
2024-10-17 12:19:05 -0300

    Atualização e acréscimo de uma função

--- login/script/login.js
+++ login/script/login.js
@@ -2,31 +2,28 @@ document.getElementById('dados-glpi').addEventListener('submit',
     function(event){
         event.preventDefault();
         
-        // Variáveis
+        // Variáveis. Tudo aqui será usado para fazer a requisição na API
 
         const formData = new FormData(this);
         const login = formData.get('login');
         const password = formData.get('password')
-        const appToken = 'xrRDRKJcXpZQqJnSSWZZGSz4RMxSbcI3RY5ytUfV';
-        const apiUrl = 'http://54.174.195.170//glpi/apirest.php/'
-        const authorization = 'PaUup1PQE08T1hmOlwOb2oEbWJ6sl00LC3c3ZoaK' 
+        const apiUrl = 'http://localhost/apirest.php/'
         const initSessionUrl = `${apiUrl}initSession?login=${login}&password=${password}`;
 
-        // Solicitação API
+        // Solicitação para a API. Vai usar o comando fetch para fazer essa requisição e vai dar um retorno 
 
         const headers = new Headers();
         headers.append('Content-Type', 'application/json');
-        headers.append('Authorization', `Basic ${authorization}`);
-        headers.append('App-Token', appToken);
         fetch(initSessionUrl, {
             method: 'GET',
-            headers: headers
+            headers: headers,
         })
 
-        // Resultados requisição API
+        // Resultados requisição API. Se a API der um retorno positivo, encaminha para a próxima página, se não der um retorno positivo ou não der retorno, gera um alerta na tela
 
         .then(response => {
             if (!response.ok) {
+                alert('Não foi possível fazer a requisição')
                 throw new Error('Erro ao realizar a busca');
             }
             return response.json();
--- pesquisaTicket/script/pesquisaTicket.js
+++ pesquisaTicket/script/pesquisaTicket.js
@@ -4,17 +4,29 @@ document.getElementById('pesquisaTicket').addEventListener('submit',
 
     // Variáveis, função & Teste
 
-    const formData = new FormData(this);
+    const formData = new FormData(this); 
     const ticketId = formData.get('id');
-    const appToken = 'xrRDRKJcXpZQqJnSSWZZGSz4RMxSbcI3RY5ytUfV';
-    const apiUrl = 'http://54.174.195.170//glpi/apirest.php/';
+    const apiUrl = 'http://localhost/apirest.php/';
     const itemTypeUrl = `${apiUrl}Ticket/${ticketId}?expand_dropdowns`;
     const urlParams = new URLSearchParams(window.location.search);
     const sessionToken = urlParams.get('token');
     const killSessionUrl = `${apiUrl}killSession`;
+    const usuariosMap = {
+      2: 'glpi',
+      3: 'post-only',
+      4: 'tech',
+      5: 'normal'
+    };
+
+    // Função para converter o ID do usuário em nome
+    function nomeUsuario(idUsuario) {
+      return usuariosMap[idUsuario] || 'Usuário desconhecido';
+    }
 
     console.log(sessionToken)
     console.log(itemTypeUrl)
+
+    // Vai pegar o número que vai retornar na API e transformar em um status equivalente ao do sistema
 
     function Status(status) {
       switch (status) {
@@ -35,6 +47,8 @@ document.getElementById('pesquisaTicket').addEventListener('submit',
       }
     }
 
+    // Vai transformar a data em dd/mm/yyyy hh:mm:ss, ao invés do formato que retorna originalmente, que é yyyy-mm-dd hh:mm:ss
+
     function formatarData(dataOriginal) {
       const partesData = dataOriginal.split(/[- ]/); // Divide a data em partes (ano, mês, dia)
       const dataFormatada = `${partesData[2]}/${partesData[1]}/${partesData[0]} ${partesData[3]}`; // Formata no formato dd/mm/yyyy
@@ -46,7 +60,6 @@ document.getElementById('pesquisaTicket').addEventListener('submit',
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     headers.append('Session-Token', `${sessionToken}`);
-    headers.append('App-Token', appToken);
 
     fetch(itemTypeUrl, {
       method: 'GET',
@@ -57,20 +70,22 @@ document.getElementById('pesquisaTicket').addEventListener('submit',
 
       .then(response => {
         if (!response.ok) {
-          // killSession();
-          // alert(erroAutenicacao);
+          alert('Erro ao realizar busca')
           throw new Error('Erro ao realizar a busca');
         }
         return response.json();
       })
 
       .then(data => {
-        const dados = JSON.stringify(data);
+        const dados = JSON.stringify(data); // Vai transformar o JSON em uma string que pode ser interpretado pelo JS
         console.log(dados);
 
         const tableBody = document.querySelector('#tabelaTickets tbody');
-        tableBody.innerHTML = null; // Limpar conteúdo antigo da tabela
-        if (Array.isArray(data)) {
+        while (tableBody.firstChild) {
+          tableBody.removeChild(tableBody.firstChild);
+        } // Limpar conteúdo antigo da tabela
+
+        if (Array.isArray(data)) { // Se for feita a pesquisa de mais de um ticket de uma vez, essa função irá formatar todos os tickets de forma igual
           data.forEach(ticket => {
             const row = tableBody.insertRow();
             row.insertCell().textContent = ticket.id;
@@ -78,14 +93,14 @@ document.getElementById('pesquisaTicket').addEventListener('submit',
             row.insertCell().textContent = formatarData(ticket.date_mod);
             row.insertCell().textContent = formatarData(ticket.date);
             row.insertCell().textContent = Status(ticket.status);
-            row.insertCell().textContent = ticket.users_id_recipient;
-            row.insertCell().textContent = ticket.users_id_lastupdater;
+            row.insertCell().textContent = nomeUsuario(ticket.users_id_recipient);
+            row.insertCell().textContent = nomeUsuario(ticket.users_id_lastupdater);
             row.insertCell().textContent = ticket.entities_id;
             const limpezaTela = document.querySelector('#tabelaTickets tbody')
           })
         }
 
-        else {
+        else { // Se retornar apenas um ticket, vai formatá-lo da ofrma certa
           const row = tableBody.insertRow();
           row.insertCell().textContent = data.id;
           row.insertCell().textContent = data.name;
@@ -98,24 +113,23 @@ document.getElementById('pesquisaTicket').addEventListener('submit',
         }
       })
 
-    const botaoKillSession = document.getElementById('logout')
+    const botaoKillSession = document.getElementById('logout') // Vai matar a sessão e vai retornar para a página de login
     botaoKillSession.addEventListener('click', function () {
       fetch(killSessionUrl, {
         method: 'GET',
         headers: headers
       })
 
-        .then(response => {
-          if (!response.ok) {
-            console.log('Erro ao matar sessão')
-          }
-          else {
-            alert('Sessão finalizada. Voltando para a página inicial')
-            console.log('Sessão finalizada')
-            window.location.href = '../index.html'
-          }
-        })
+      .then(response => { 
+        if (response.ok) {
+          alert('Sessão finalizada. Voltando para a página inicial')
+          console.log('Sessão finalizada')
+          window.location.href = '../index.html' 
+        }
+        else {
+          alert('Erro ao matar sessão') 
+        }
+      })
     })
   }
 )
-
